5cd65d2589da2a43150308a6a5aaac26d868437f
Oluwaseun011 <oluwaseunidowu011@gmail.com>
2022-02-11 12:35:51 +0100

    awwal

---
+++ awwalChal/num1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace num1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sub("ab", "eidboaoo");
+        }
+
+        public static void Sub(string s1, string s2)
+        {
+            int a = s1.Length;
+            int b = s2.Length;
+            int count = 0;
+            List<string> perms = Perm(s1, 0, a - 1);
+            var substrings = new List<string>();
+
+
+            for (int i = 0; i <= b - a; i++)
+            {
+                string etf = s2.Substring(i, a);
+                substrings.Add(etf);
+            }
+
+
+            for (int k = 0; k <= substrings.Count - 1; k++)
+            {
+                for (int l = 0; l <= perms.Count - 1; l++)
+                {
+                    if (perms[l] == substrings[k])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            if (count > 0)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+
+        public static List<string> Perm(string s, int x, int y)
+        {
+            List<string> permList = new List<string>();
+
+            if (x == y)
+            {
+                permList.Add(s);
+
+            }
+
+            else
+            {
+                for (int i = x; i <= y; i++)
+                {
+                    s = Sort(s, x, i);
+                    Perm(s, x + 1, y);
+                    permList.Add(s);
+                }
+
+            }
+
+            return permList;
+        }
+
+        public static int Fact(int b)
+        {
+            int fact = 1;
+            for (int i = b; i > 0; i--)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+
+        public static string Sort(string asd, int i, int j)
+        {
+            var qwe = asd.ToCharArray();
+            char temp = qwe[i];
+            qwe[i] = qwe[j];
+            qwe[j] = temp;
+            return new string(qwe);
+
+        }
+    }
+}
---
+++ awwalChal/num2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace num2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void Ok(int num)
+        {
+            char l = '(';
+            char r = ')';
+            int a = (num * 2) - 1;
+            var asd = new List<string>();
+            var x = new Stack();
+            var y = new Stack();
+            string s = "";
+            for (int i = 0; i <= num; i++)
+            {
+                x.Push(l);
+                y.Push(r);
+            }
+
+            for (int j = 1; j <= a; j++)
+            {
+                s += x.Peek();
+                s += y.Peek();
+            }
+
+
+
+
+
+        }
+    }
+}
